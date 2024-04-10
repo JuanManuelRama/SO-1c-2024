c545cfcba51380a3fe3aa1f3f080d9b88798bc00
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-10 19:00:00 +0000

    Añadí @return y 2 typedef

--- utils/src/sockets/servidor.h
+++ utils/src/sockets/servidor.h
@@ -10,10 +10,21 @@
 #include<string.h>
 #include<assert.h>
 
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+extern t_log* logger;
+
+
 /**
 * @fn    iniciar_servidor
 * @brief Inicializa el servidor
 * @par   Ninguno
+* @return  Socket del Servidor
 */
 int iniciar_servidor(void);
 
@@ -21,6 +32,7 @@ int iniciar_servidor(void);
 * @fn    esperar_cliente
 * @brief Espera al cliente
 * @par   Socket del Servidor
+* @return Socket del Cliente
 */
 int esperar_cliente(int)
 
@@ -28,6 +40,7 @@ int esperar_cliente(int)
 * @fn    recibir_paquete
 * @brief Recibe el paquete
 * @par   Socket del Cliente
+* @return Paquete
 */
 t_list* recibir_paquete(int);
 
@@ -35,6 +48,7 @@ t_list* recibir_paquete(int);
 * @fn    recibir_mensaje
 * @brief Recibe el Mensaje
 * @par   Socket del Cliente
+* @return Nada
 */
 void recibir_mensaje(int);
 
@@ -42,6 +56,7 @@ void recibir_mensaje(int);
 * @fn    recibir_operacion
 * @brief Recibe la Operación
 * @par   Socket del Cliente
+* @return Código de Operación, o -1 en caso de error
 */
 int recibir_operacion(int);
 
