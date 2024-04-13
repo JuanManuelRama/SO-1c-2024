3dddc27853ff0f85044b5054fc68cfd5a6e7d339
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-13 23:11:52 +0000

    Cree la funcion para que se conecten los clientes

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,8 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <sockets/servidor.h>
+#include <sockets/servidor.c>
 
-int main(int argc, char* argv[]) {
-    decir_hola("Memoria");
+void conectCliente(t_log* logger, int socket_servidor, char* cliente, int socket_cliente)
+{
+	log_info(logger, "Esperando a "); //No se como concatenar el mensaje al nombre del cliente 
+	socket_cliente = esperar_cliente(socket_servidor); //No se como pasar el socket del cliente por referencia
+}
+
+int main(void){
+	t_log* logger;
+	int socket_servidor;
+	int socket_Kernel=0;
+
+	logger = log_create ("logM.log", "Crear log",1, LOG_LEVEL_INFO);
+	socket_servidor = iniciar_servidor();
+	log_info(logger, "Memoria inicializada");
+	conectCliente(logger, socket_servidor, "Kernel", socket_Kernel);
+	log_destroy(logger);
     return 0;
 }
