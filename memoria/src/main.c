805e9f77f7fa2a76732d985a993c8cdf0bda7ce5
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-14 23:31:24 +0000

    Conexión exitosa al Kernel

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <utils/hello.h>
 #include <sockets/servidor.c>
+#include <utils/varias.h>
 
 
 int main(void){
@@ -10,14 +11,17 @@ int main(void){
 	char* puerto;
 	int socket_servidor;
 	int socket_cpu;
+	int socket_kernel;
 
 	logger = log_create ("logM.log", "Crear log",1, LOG_LEVEL_INFO);
 	config=config_create("memoria.config");
-		if(config_has_property(config, "PUERTO_ESCUCHA"))
-		puerto = config_get_string_value(config, "PUERTO_ESCUCHA");
+	puerto = buscar(logger, config, "PUERTO_ESCUCHA");
 	log_info(logger, "Memoria inicializada");
 	socket_servidor = iniciar_servidor(logger, puerto);
 	socket_cpu = esperar_cliente(logger, "CPU", socket_servidor);
+	socket_kernel = esperar_cliente(logger, "Kernel", socket_servidor);
+
 	log_destroy(logger);
+	config_destroy(config);
     return 0;
 }
