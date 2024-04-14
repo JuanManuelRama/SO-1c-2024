3770a810035c5a50fc579bad74386cfbaaa52ad3
ValSpada <vspada@frba.utn.edu.ar>
2024-04-14 21:56:07 +0000

    Establecida la primera conexion

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,8 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <sockets/cliente.c>
 
 int main(int argc, char* argv[]) {
-    decir_hola("CPU");
+
+    int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+    logger = log_create("logCpu.log", "LOGS CPU", 1, LOG_LEVEL_INFO);
+    log_info(logger, "TEST", "INFO");
+    
+    config = config_create("cpu.config");
+	
+	if(config_has_property(config, "IP_MEMORIA"))
+		ip = config_get_string_value(config, "IP_MEMORIA");
+
+	if(config_has_property(config, "PUERTO_MEMORIA"))
+		puerto = config_get_string_value(config, "PUERTO_MEMORIA");
+
+    log_info(logger, ip, "INFO");
+	log_info(logger, puerto, "INFO");
+
+	conexion = crear_conexion(ip, puerto);
+
+    liberar_conexion(conexion);
+    log_destroy(logger);
+    config_destroy(config);
     return 0;
 }
--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
-#include <sockets/servidor.h>
 #include <sockets/servidor.c>
 
 
