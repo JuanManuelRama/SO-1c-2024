f1a3bee60dd2c705c41adcef6f3a407ffd77f5f2
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-14 15:26:38 +0000

    Arreglé el log info de conectCliente

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -6,19 +6,19 @@
 
 void conectCliente(t_log* logger, int socket_servidor, char* cliente, int socket_cliente)
 {
-	log_info(logger, "Esperando a "); //No se como concatenar el mensaje al nombre del cliente 
+	log_info(logger, "Esperando a %s\n", cliente); //No se como concatenar el mensaje al nombre del cliente 
 	socket_cliente = esperar_cliente(socket_servidor); //No se como pasar el socket del cliente por referencia
 }
 
 int main(void){
 	t_log* logger;
 	int socket_servidor;
-	int socket_Kernel=0;
+	int socket_cpu=0;
 
 	logger = log_create ("logM.log", "Crear log",1, LOG_LEVEL_INFO);
 	socket_servidor = iniciar_servidor();
 	log_info(logger, "Memoria inicializada");
-	conectCliente(logger, socket_servidor, "Kernel", socket_Kernel);
+	conectCliente(logger, socket_servidor, "CPU", socket_cpu);
 	log_destroy(logger);
     return 0;
 }
