b8137912703b70c694c09462d12e33feeae90ea5
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-14 23:30:49 +0000

    Emprolijé el codigo, intento de conexión al kernel

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <utils/hello.h>
 #include <sockets/cliente.c>
+#include <utils/varias.h>
 
 int main(int argc, char* argv[]) {
 
     int conexion;
+	int socket_servidor;
+	int socket_kernel;
 	char* ip;
 	char* puerto;
 	char* valor;
-
 	t_log* logger;
 	t_config* config;
 
@@ -18,16 +20,16 @@ int main(int argc, char* argv[]) {
     
     config = config_create("cpu.config");
 	
-	if(config_has_property(config, "IP_MEMORIA"))
-		ip = config_get_string_value(config, "IP_MEMORIA");
+	ip = buscar(logger, config, "IP_MEMORIA");
 
-	if(config_has_property(config, "PUERTO_MEMORIA"))
-		puerto = config_get_string_value(config, "PUERTO_MEMORIA");
+	puerto = buscar(logger, config, "PUERTO_MEMORIA");
 
     log_info(logger, ip, "INFO");
 	log_info(logger, puerto, "INFO");
 
 	conexion = crear_conexion(ip, puerto);
+	socket_servidor = iniciar_servidor(logger, "8006");
+	socket_kernel = esperar_cliente(logger, "Kernel", socket_servidor);
 
     liberar_conexion(conexion);
     log_destroy(logger);
