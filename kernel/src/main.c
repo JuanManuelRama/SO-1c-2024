b71afa7f1414e3cf55d04e290d5bd61623ba81a3
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-28 15:58:50 +0000

    La consola del kernel esta funcional, codigo emprolijado

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,69 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <utils/hello.h>
-#include <sockets/sockets.c>
-#include <pthread.h>
+#include <kernel/kernel.h>
 
-typedef struct
-{
-	uint32_t PC;
-	uint8_t AX;
-	uint8_t BX;
-	uint8_t CX;
-	uint8_t DX;
-	uint32_t EAX;
-	uint32_t EBX;
-	uint32_t ECX;
-	uint32_t EDX;
-	uint32_t SI;
-	uint32_t DI;
-} t_registros;
-
-typedef struct 
-{
-	int pid;
-	int pc;
-	int quantum;
-	t_registros registros;
-	char* estado;
-} t_pcb;
-
+//Variables globales
 t_log* logger;
 t_config* config;
-
-void enviar_Proceso(int socket_cliente)
-{
-	t_paquete* paquete = malloc(sizeof(t_paquete));
-
-	paquete->codigo_operacion = NUEVO_PROCESO;
-	paquete->buffer = malloc(sizeof(t_buffer));
-	paquete->buffer->size = strlen("instru.txt") + 1;
-	paquete->buffer->stream = malloc(paquete->buffer->size);
-	memcpy(paquete->buffer->stream, "instru.txt", paquete->buffer->size);
-
-	int bytes = paquete->buffer->size + 2*sizeof(int);
-
-	void* a_enviar = serializar_paquete(paquete, bytes);
-
-	send(socket_cliente, a_enviar, bytes, 0);
-
-	free(a_enviar);
-	eliminar_paquete(paquete);
-}
+int conexion_memoria;
+int conexion_cpu;
 
 
 int main(int argc, char* argv[]) {
-    int conexion_memoria;
-    int conexion_cpu;
 	int kernel_servidor;
 	int socket_IO;
 	char* ip;
 	char* puerto;
 	pthread_t hilo_IO;
 
-	// inicializamos logger y config
-	logger = log_create("logKernel.log", "LOGS Kernel", 1, LOG_LEVEL_INFO);
-	config = config_create("kernel.config");
+	inicializar();
     
 	// buscamos datos en config y conectamos a memoria
 	ip = buscar("IP_MEMORIA");
@@ -72,24 +23,57 @@ int main(int argc, char* argv[]) {
 	enviar_mensaje("Saludos desde el Kernel",conexion_memoria);
 
 	// buscamos datos en config y conectamos a cpu
-	/*ip = buscar("IP_CPU");
-	puerto = buscar("PUERTO_CPU_DISPATCH");
-	conexion_cpu = crear_conexion(ip, puerto, "CPU");
-	enviar_mensaje("Saludos desde el Kernel",conexion_cpu);
+	//ip = buscar("IP_CPU");
+	//puerto = buscar("PUERTO_CPU_DISPATCH");
+	//conexion_cpu = crear_conexion(ip, puerto, "CPU");
+	//enviar_mensaje("Saludos desde el Kernel",conexion_cpu);
 
 	//tambien sera servidor, con el I/O como cliente
-	puerto = buscar("PUERTO_ESCUCHA");
-	kernel_servidor = iniciar_servidor(puerto, "Kernel");
+	//puerto = buscar("PUERTO_ESCUCHA");
+	//kernel_servidor = iniciar_servidor(puerto, "Kernel");
 
-	socket_IO = esperar_cliente("I/O", kernel_servidor);
-	pthread_create(&hilo_IO, NULL, interactuar, (void*)socket_IO);
+	//socket_IO = esperar_cliente("I/O", kernel_servidor);
+	//pthread_create(&hilo_IO, NULL, interactuar, (void*)socket_IO);
+
+
+	char* buffer;
+	char** mensaje;
+	int consola;
+	logger = log_create("logKernel.log", "LOGS Kernel", 1, LOG_LEVEL_INFO);
+	string_new(buffer);
+	while(1){
+		buffer  = readline(">");
+		mensaje = string_split(buffer, " ");
+		consola = strcmp ("INICIAR_PROCESO", mensaje[0]);
+		switch (consola){
+			case INICIAR_PROCESO:
+				enviar_proceso (mensaje[1]);
+				break;
+			case FINALIZAR_PROCESO:
+				log_info(logger, "Proceso finalizado");
+				break;
+			case EJECUTAR_SCRIPT:
+				log_info(logger, "Script ejecutado");
+				break;
+			case DETENER_PLANIFICACION:
+				log_info(logger, "Planificacion detenida");
+				break;
+			case INICIAR_PLANIFICACION:
+				log_info(logger, "Planificacion iniciada");
+				break;
+			case PROCESO_ESTADO:
+				log_info(logger, "El estado del proceso es:");
+				break;
+			default:
+				log_info(logger, "Código invalido");
+				break;
+		}
+
+	}
 
 	pthread_join(hilo_IO, NULL);
-	*/
-	enviar_Proceso(conexion_memoria);
-	log_destroy(logger);
-	config_destroy(config);
-	liberar_conexion(conexion_memoria);
-	//liberar_conexion(conexion_cpu);
+	finalizar();
+
+	
     return 0;
 }
