ea80678ac203b0eb1bf66fdd1acf59a94db8d378
JuanManuelRama <jrama@frba.utn.edu.ar>
2024-04-28 16:33:41 +0000

    Emprolijo codigo

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <memoria/memoria.h>
 
 int TAM_MEMORIA;
 short CANT_PAG;
@@ -10,39 +10,7 @@ void* memoria_contigua;
 t_log* logger;
 t_config* config;
 
-t_list* lProcesos;
 t_tablaMemoria (*tablaMemoria);
-
-
-void recibir_proceso(int socket_cliente){
-		int size;
-		char* proceso = recibir_buffer (&size, socket_cliente);
-		log_info(logger, proceso);
-		cargar_proceso(proceso);
-}
-
-
-void interactuar_Kernel(int socket_cliente){
-		while (1) {
-		int cod_op = recibir_operacion(socket_cliente);
-		switch (cod_op) {
-		case MENSAJE:
-			recibir_mensaje(socket_cliente);
-			break;
-		case NUEVO_PROCESO:
-			recibir_proceso(socket_cliente);
-			break;
-		case -1:
-			log_error(logger, "el cliente se desconecto");
-			return EXIT_FAILURE;
-		default:
-			log_warning(logger,"Operacion no esperada por parte de este cliente");
-			break;
-		}
-	}
-}
-
-
 
 int main (){
 	char* puerto;
@@ -52,7 +20,7 @@ int main (){
 	int socket_IO;
 	pthread_t hilo_cpu, hilo_kernel, hilo_IO;
 
-	inicializar();
+	inicializar_memoria();
 
 	// buscamos dato en config e inicializamos servidor para cpu y kernel como clientes
 	puerto = buscar("PUERTO_ESCUCHA");
@@ -71,8 +39,6 @@ int main (){
 	//cargar_proceso("instru.txt");
 	//Enviar la lProcesos al Kernel para que la añada al PCB
 	pthread_join(hilo_kernel, NULL);
-	t_instruccion *t_instruccion = list_get(lProcesos, 4);
-	log_info(logger, "%s", t_instruccion->instruccion);
 
 
 
@@ -80,63 +46,7 @@ int main (){
 
 	//pthread_join(hilo_IO, NULL);
 
-	finalizar();
+	finalizar_memoria();
 	return 0;
 }
 
-void inicializar_tabla_de_memoria(){
-    for(short i = 0; i < CANT_PAG; i++){
-    	tablaMemoria[i].ocupado=false;
-		tablaMemoria[i].espacio_libre=TAM_PAG;
-	}
-}
-
-void inicializar(){
-	logger = log_create ("logM.log", "LOGS MEMORIA",1, LOG_LEVEL_INFO); 
-	config = config_create("memoria.config"); 
-
-	TAM_PAG = config_get_int_value(config,"TAM_PAGINA");
-	TAM_MEMORIA = config_get_int_value(config,"TAM_MEMORIA");
-
-	//PATH_INSTRUCCIONES = buscar("PATH_INSTRUCCIONES");
-
-	if (TAM_MEMORIA % TAM_PAG) {
-		log_error(logger, "Tamaño de memoria no multiplo de cantidad de paginas");
-		exit(-1);
-	}
-
-	CANT_PAG = TAM_MEMORIA/TAM_PAG;
-	memoria_contigua = malloc(TAM_MEMORIA);
-	tablaMemoria = malloc(CANT_PAG*sizeof(t_tablaMemoria));
-	inicializar_tabla_de_memoria();
-}
-
-void cargar_proceso(char* nombreArchivo){
-	// aca se podria agregar el uso de PATH_INSTRUCCIONES
-	FILE* archivoProceso = fopen(nombreArchivo, "r");
-
-	lProcesos = list_create();
-	t_instruccion *tInstruccion;
-	char* buffer;
-	buffer=malloc(MAX_LINEA);
-
-	// recorre archivo cargando linea por linea en la lista
-	while (!feof(archivoProceso)){
-		tInstruccion=malloc(sizeof(t_instruccion));
-		fgets(buffer, MAX_LINEA, archivoProceso);
-		tInstruccion->longitud=strlen(buffer)-1;
-		strncpy(tInstruccion->instruccion, buffer, tInstruccion->longitud);
-		list_add(lProcesos, tInstruccion);
-	}
-
-	free (buffer);
-	fclose(archivoProceso);
-}
-
-void finalizar(){
-	free(memoria_contigua);
-	free(tablaMemoria);
-	list_destroy(lProcesos);
-	log_destroy(logger);
-	config_destroy(config);
-}
